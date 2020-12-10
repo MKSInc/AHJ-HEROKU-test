be7a51236692469cffc575dfe5e2167a0f3ad174
Maksim <pyshkov.maksim@gmail.com>
2020-12-10 15:54:35 +0200

    testing...

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+###[HEROKU-server link](https://heroku-server-node-js-test.herokuapp.com)
 ## AHJ-HEROKU-test
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const http = require('http');
+const PORT = process.env.PORT || 7070;
 
 const server = http.createServer((req, res) => {
-   res.end('Server response' +
-      '\nURL: ' + req.url);
+   res.end(`Server has been started on port ${PORT} ...
+   \nRequested url: ${req.url}`);
 });
-
-const PORT = process.env.PORT || 7070;
 
 server.listen(PORT, () => {
    console.log(`Server has been started on port ${PORT} ...`);
