8a290d5d67b768a025b2850a0fa8d3a963fad482
Maksim <pyshkov.maksim@gmail.com>
2020-12-29 18:43:30 +0200

    feat: testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-###[HEROKU-server link](https://heroku-server-node-js-test.herokuapp.com)
+### [HEROKU-server link](https://heroku-server-node-js-test.herokuapp.com)
 ## AHJ-HEROKU-test
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const http = require('http');
 const PORT = process.env.PORT || 7070;
 
 const server = http.createServer((req, res) => {
+   console.log('---------------------------');
+   console.log(req.url);
+   if (req.method === 'OPTIONS') {
+      console.log('OPTIONS');
+   }
+   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'PUT, DELETE',});
    res.end(`Server has been started on port ${PORT} ...
-   \nRequested url: ${req.url}`);
+   \nRequested url: ${req.url}
+   \nRequested method: ${req.method}`);
 });
 
 server.listen(PORT, () => {
