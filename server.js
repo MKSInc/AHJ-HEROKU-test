d7ab2effd7aa0f079f0b4d09d18b7d7e4e69c4d4
Maksim <max1983@inbox.lv>
2021-09-22 18:33:25 +0300

    test: committing

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const http = require('http');
 const PORT = process.env.PORT || 7070;
 
 const server = http.createServer((req, res) => {
+   console.log('---------------------------');
+   console.log(req.url);
+   if (req.method === 'OPTIONS') {
+      console.log('OPTIONS');
+   }
+   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'PUT, DELETE',});
    res.end(`Server has been started on port ${PORT} ...
-   \nRequested url: ${req.url}`);
+   \nRequested url: ${req.url}
+   \nRequested method: ${req.method}`);
 });
+
+// Test committing
 
 server.listen(PORT, () => {
    console.log(`Server has been started on port ${PORT} ...`);
